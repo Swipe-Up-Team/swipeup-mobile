1d067371fa120495a322f96f325dbf01a70fe4f1
Vo Hoang Duc Khoa <19520646@gm.uit.edu.vn>
2022-04-22 16:02:07 +0700

    add alias for absolute paths

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@src': './src',
+            '@components': './src/components',
+            '@store': './src/store',
+            '@assets': './assets'
+          }
+        }
+      ]
+    ]
   }
 }
