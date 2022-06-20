c1f121e858fa8ba0887ed235eb976460b486ca7f
Johnny-Luu <ngocsangluu58@gmail.com>
2022-06-20 22:00:07 +0700

    block self-message

--- src/screens/profile/profile.tsx
+++ src/screens/profile/profile.tsx
@@ -1,14 +1,12 @@
 /* eslint-disable react-native/no-inline-styles */
 import { RouteProp, useRoute } from '@react-navigation/native'
 import { useSelector } from '@src/common'
-import { AddPostCard, PostCard, StyledDivider } from '@src/components'
+import { PostCard, StyledDivider } from '@src/components'
 import { defaultUser } from '@src/constants'
 import { Post, User } from '@src/models'
-import { navigate } from '@src/navigation/navigation-service'
-import { APP_SCREEN, AuthorizeParamsList, RootStackParamList } from '@src/navigation/screen-types'
+import { APP_SCREEN, AuthorizeParamsList } from '@src/navigation/screen-types'
 import { postService, userService } from '@src/services'
-import { Icon, Spinner } from '@ui-kitten/components'
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { RefreshControl, Text, View } from 'react-native'
 import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import { SafeAreaView } from 'react-native-safe-area-context'
@@ -25,7 +23,7 @@ export default function ProfileScreen({ navigation }: any) {
   const [allPost, setAllPost] = useState<Post[]>([])
   const [refreshing, setRefreshing] = useState(false)
 
-  const handleLoadMore = async () => {}
+  // const handleLoadMore = async () => {}
 
   const renderFooter = () => {
     // if (!loading || !hasMoreToLoad) return null
@@ -37,11 +35,11 @@ export default function ProfileScreen({ navigation }: any) {
     return null
   }
 
-  const onFollow = () => {}
+  // const onFollow = () => {}
 
-  const onUnfollow = () => {}
+  // const onUnfollow = () => {}
 
-  const onClickFollow = () => {}
+  // const onClickFollow = () => {}
 
   const getFriendUser = async () => {
     const friend = await userService.getUser(friendUserId!)
@@ -89,7 +87,9 @@ export default function ProfileScreen({ navigation }: any) {
               <Text style={styles.emailText}>{currentUser.email}</Text>
             </View>
 
-            {friendUserId && <ActionButtonRow currentUser={currentUser} navigation={navigation} />}
+            {friendUserId && friendUserId !== user?.id && (
+              <ActionButtonRow currentUser={currentUser} navigation={navigation} />
+            )}
 
             <View style={styles.extraInfoWrapper}>
               <TouchableOpacity style={styles.touch_center}>
